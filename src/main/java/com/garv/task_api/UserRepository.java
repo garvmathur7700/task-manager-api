39c5c0c083f58bb3d9bf664e2de1ad59d3ed5f8c
Garv Mathur <garvmathur7700@gmail.com>
2025-09-02 23:42:42 +0530

    removed tracked files that are now in gitignore

--- src/main/java/com/garv/task_api/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.garv.task_api;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-
-public interface UserRepository extends MongoRepository<User, String> {
-    Optional<User> findByUsername(String username);
-}
